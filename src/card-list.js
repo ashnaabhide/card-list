2b56a4e5a7616616c335db674fd92ea9b0549308
ashnaabhide <32545625+ashnaabhide@users.noreply.github.com>
2023-03-05 18:23:59 -0500

    url fix

--- src/card-list.js
+++ src/card-list.js
@@ -4,7 +4,7 @@ import 'meme-card-2/src/meme-card-2';
 import 'meme-card-ashnaa/src/meme-card-ashnaa';
 
 
-const meme=new URL('../assets/meme.jpg', import.meta.url).href;
+//const meme=new URL('../assets/meme.jpg', import.meta.url).href;
 
 export class CardList extends LitElement {
   static properties = {
